dc84c904bc0a3f5208a3819edc43fe9a96b5c31e
kangqiao-ctrl <kangqiao2015@gmail.com>
2021-04-19 18:17:30 -0230

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='EnvCausal',
     packages=find_packages(),
     version='0.3.0',
+    description = "A Causal Inference Framework for Environmental Data Analysis",
+    author = "kangqiao-ctrl",
+    author_email = "qiaok@mun.ca",
+    url = "https://github.com/kangqiao-ctrl/EnvCausal",
     install_requires=[          
         'pandas',
         'numpy',
